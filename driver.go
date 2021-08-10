a08e519d91abf5ce3bad5276d9e9965c0272341a
duhaoming <741489219@qq.com>
2021-08-10 15:36:34 +0800

    添加清理自身

--- driver.go
+++ driver.go
@@ -11,8 +11,10 @@ type driverConn struct {
 	mu sync.Mutex
 
 	createdAt time.Time //连接创建时间
-	inUse     bool      //是否被使用
 	ci        io.Closer //连接接口
+	done      chan struct{}
+	isClosed  bool
+	inUse     bool //是否被使用
 }
 
 // 判断活跃时间是否到期
@@ -24,16 +26,22 @@ func (dc *driverConn) expired(timeout time.Duration) bool {
 }
 
 // 关闭资源
-func (dc *driverConn) close() error {
-	var err error
-
+func (dc *driverConn) close() (err error) {
 	dc.mu.Lock()
-	if dc.ci == nil {
+	if dc.ci == nil || dc.isClosed {
 		dc.mu.Unlock()
 		return nil
 	}
-	err = dc.ci.Close()
+	dc.isClosed = true
+	if dc.done == nil {
+		closedchan := make(chan struct{})
+		close(closedchan)
+		dc.done = closedchan
+	} else {
+		close(dc.done)
+	}
 	dc.mu.Unlock()
+	err = dc.ci.Close()
 
 	dc.db.Lock()
 	dc.db.numOpen--
@@ -50,11 +58,56 @@ func (dc *driverConn) Conn() io.Closer {
 
 // 回收资源
 func (dc *driverConn) Close() error {
-        dc.db.Lock()
-        if !dc.db.recovery(dc) {
-                dc.db.Unlock()
-                return dc.close()
-        }   
-        dc.db.Unlock()
-        return nil 
+	if !dc.db.putConn(dc) {
+		return dc.close()
+	}
+	return nil
+}
+
+func (dc *driverConn) cleanDriver() {
+
+	t := time.NewTimer(dc.db.maxLifetime)
+	defer t.Stop()
+	for {
+		select {
+		case <-t.C:
+		case <-dc.Done():
+			return
+		}
+		dc.db.Lock()
+		if dc.db.closed || dc.db.numOpen == 0 {
+			dc.db.releaseConnRequests()
+			dc.db.Unlock()
+			return
+		}
+
+		var closing []*driverConn = make([]*driverConn, 0, 1)
+		for i, c := range dc.db.freeConn {
+			if c == dc {
+				closing = append(closing, c)
+				last := len(dc.db.freeConn) - 1
+				dc.db.freeConn[i] = dc.db.freeConn[last]
+				dc.db.freeConn[last] = nil
+				dc.db.freeConn = dc.db.freeConn[:last]
+				break
+			}
+		}
+		dc.db.Unlock()
+
+		for _, c := range closing {
+			c.close()
+			return
+		}
+		t.Reset(dc.db.maxLifetime)
+	}
+}
+
+func (dc *driverConn) Done() <-chan struct{} {
+	dc.mu.Lock()
+	if dc.done == nil {
+		dc.done = make(chan struct{})
+	}
+	d := dc.done
+	dc.mu.Unlock()
+	return d
 }
