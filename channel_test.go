8614c9f205cff9ff32d5cef29dd8c747f1b7a69d
duhaoming <741489219@qq.com>
2021-10-17 12:42:37 +0800

    解决tcp连接粘包问题

--- channel_test.go
+++ channel_test.go
@@ -1,142 +1,140 @@
-// 一般不用做tcp连接池, 会发生粘包丢包情况
 // 用作多的是数据库的连接池
-// 或者文件打开的池、http连接池
 
 package pool
 
 import (
-	"bytes"
+	"bufio"
 	"context"
+	"fmt"
 	"io"
-	"log"
 	"net"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 )
 
 var addr = "127.0.0.1:8061"
 
-func Connects(_ context.Context) (io.Closer, error) {
-	return net.Dial("tcp", addr)
-}
-
-func testListen() (func(), error) {
-	listen, err := net.Listen("tcp", addr)
-	if err != nil {
-		return func() {}, err
-	}
-
-	go Accetp(listen)
-	return func() {
-		listen.Close()
-	}, nil
-}
-
-func Accetp(listen net.Listener) {
-	for {
-		conn, err := listen.Accept()
-
-		if err != nil {
-			println(err.Error(), "------")
-			return
-		}
-		defer conn.Close()
-
-		go func() {
-			for {
-				read := make([]byte, 10)
-				var rsp bytes.Buffer
-				rsp.WriteString("hello ")
-				if _, err := conn.Read(read); err != nil {
-					//log.Printf("server read: %v\n", err)
-					return
-				}
-				rsp.Write(read)
-				if _, err := conn.Write(rsp.Bytes()); err != nil {
-					//log.Printf("server write: %v\n", err)
-					return
-				}
-			}
-		}()
-	}
-
-}
-
 func TestPool(t *testing.T) {
-	cancel, err := testListen()
-	if err != nil {
-		t.Fatal(err)
-	}
+	cancel := server()
+	defer cancel()
 
-	poolConn := OpenCustom(Connects, time.Minute*5, time.Second*5, 10, 10)
-	c, err := poolConn.Get()
+	b := append([]byte("duhaoming"), '\n') // 数据包的区分\n
+	tp := testPool()
+	defer tp.Close()
+	c, err := tp.Get()
 	if err != nil {
 		t.Fatal(err)
 	}
-	co := c.Conn().(net.Conn)
-	if _, err := co.Write([]byte("world")); err != nil {
+	defer c.Close()
+	conn := c.Conn().(net.Conn)
+	if _, err := conn.Write(b); err != nil {
 		t.Fatal(err)
 	}
-	read := make([]byte, 12)
-	if _, err := co.Read(read); err != nil {
+	read := make([]byte, 2)
+	if _, err := conn.Read(read); err != nil {
 		t.Fatal(err)
 	}
-	println(string(read))
-	c.Close()
-	poolConn.Close()
-	cancel()
 }
 
 func BenchmarkGetPool(b *testing.B) {
-	b.StopTimer()
-	cancel, err := testListen()
-	if err != nil {
-		b.Fatal(err)
-	}
+	cancel := server()
+	defer cancel()
+	atomic.SwapUint64(&count, 0)
+	atomic.SwapUint64(&writeCount, 0)
 
-	poolConn := OpenCustom(Connects, time.Minute*5, time.Second*5, 10, 10)
-	b.StartTimer()
+	tp := testPool()
+	sendB := append([]byte("duhaoming"), '\n')
 
 	var n sync.WaitGroup
-	println(b.N)
+	n.Add(b.N)
 	for i := 0; i < b.N; i++ {
-		n.Add(1)
-		go func(n *sync.WaitGroup) {
+		go func(i int, n *sync.WaitGroup) {
 			defer n.Done()
-			c, err := poolConn.Get()
+			c, err := tp.Get()
 			if err != nil {
-				b.Fatal(err)
+				fmt.Println("Error client", err.Error())
+				return
 			}
-
-			co := c.Conn().(net.Conn)
-
-			if _, err := co.Write([]byte("world")); err != nil {
-				b.Fatal("client write: ", err)
+			defer c.Close()
+			if conn, ok := c.Conn().(net.Conn); ok {
+				// conn.Write(append([]byte(fmt.Sprintf("%d", i)), sendB...))
+				conn.Write(sendB)
+				atomic.AddUint64(&writeCount, 1)
+				_, err := bufio.NewReader(conn).ReadBytes('\n')
+				if err != nil {
+					fmt.Println("Error response: ", err)
+					return
+				}
+				// fmt.Printf("response: %s\n", read)
 			}
-			read := make([]byte, 13)
-			if _, err := co.Read(read); err != nil {
-				b.Fatal("client read: ", err)
-			}
-			log.Printf("%s\n", read)
-			log.Printf("%s\n", co.LocalAddr().String())
-
-			c.Close()
-		}(&n)
+		}(i, &n)
 	}
 	n.Wait()
-	cancel()
-	poolConn.Close()
-
-	b.StopTimer()
+	tp.Close()
+	// fmt.Println(atomic.LoadUint64(&count), atomic.LoadUint64(&writeCount))
 }
 
 // 关闭池之后连接获取是否正常
 func TestColse(t *testing.T) {
-	poolConn := OpenCustom(Connects, time.Minute*5, time.Second*5, 10, 10)
-	poolConn.Close()
-	_, err := poolConn.Get()
+	tp := testPool()
+	tp.Close()
+	c, err := tp.Get()
 	if err != nil {
 		t.Fatal(err)
 	}
+	c.Close()
+}
+
+func server() func() error {
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Println("Error listening", err.Error())
+		return func() error { return nil } //终止程序
+	}
+	go accept(listener)
+	return listener.Close
+}
+
+func accept(listener net.Listener) {
+	for {
+		conn, err := listener.Accept()
+		if _, ok := err.(*net.OpError); ok {
+			return
+		}
+		if err != nil {
+			fmt.Println("Error accepting", err.Error())
+			return
+		}
+		go read(conn)
+	}
+}
+
+var count, writeCount uint64
+
+func read(conn net.Conn) {
+	for {
+		b, err := bufio.NewReader(conn).ReadBytes('\n')
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			fmt.Println("Error reading", err.Error())
+			return
+		}
+		// fmt.Printf("Received data: %s\n", b)
+		conn.Write(b)
+		atomic.AddUint64(&count, 1)
+	}
+	return
+}
+
+func testPool() Pool {
+	return OpenCustom(func(ctx context.Context) (io.Closer, error) {
+		dialer := &net.Dialer{
+			KeepAlive: time.Minute * 5,
+		}
+		return dialer.DialContext(ctx, "tcp", addr)
+	}, time.Minute*10, time.Second*15, 100, 100)
 }
